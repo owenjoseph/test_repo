08fa8fe7569766845d24336b62304a38d8b074c2
Owen O'Malley <owenomalley@owens-imac.home>
2019-05-29 00:27:29 -0700

    added first component

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,10 @@
 import React from "react"
 
-export default () => <div>Hello world!</div>
+export default () => (
+    <div style={{ color: `purple` }}>
+        <h1>I like the way you work it!</h1>
+        <h2>(No diggity!)</h2>
+        <p>I like to bag it up! (Bag it up!)</p>
+        <img src="https://source.unsplash.com/random/400x200" alt="" />
+    </div>
+)
