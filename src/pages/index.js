b8f553ac94829b2972ec90ec34c5c4e0b2ab0b17
Owen O'Malley <owenomalley@owens-imac.home>
2019-05-29 00:35:19 -0700

    added Link to about page to index

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,10 @@
 import React from "react"
+import { Link } from "gatsby"
+import Header from "../components/header"
 
 export default () => (
     <div style={{ color: `purple` }}>
+        <Link to="/about/">About</Link>
         <h1>I like the way you work it!</h1>
         <h2>(No diggity!)</h2>
         <p>I like to bag it up! (Bag it up!)</p>
