0ccbc4aa23b0b030a677bb397f8b96a38ae8d887
Owen O'Malley <notowen@gmail.com>
2019-05-30 17:42:06 -0700

    added contact page w links and links to home

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,9 +1,11 @@
 import React from "react"
 import Header from "../components/header"
+import { Link } from "gatsby";
 
 export default () => (
   <div>
     <Header headerText="About Gatsby" />
     <p>Such wow. Very React.</p>
+    <Link to="/">Home</Link>
   </div>
 )
